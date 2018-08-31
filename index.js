68abf29749f53b796ff7cc7f2068bb06d0de079d
YasunoriKawasaki <kawasaki@cloud-ace.jp>
2018-08-31 15:38:18 +0900

    Improve

--- index.js
+++ index.js
@@ -1,40 +1,76 @@
-"use strict";
-
-/*
-** Import Packages
-*/
+// -----------------------------------------------------------------------------
+// モジュールのインポート
 const server = require("express")();
-const bot_express = require("bot-express");
+const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
+const dialogflow = require("dialogflow");
 
-/*
-** Middleware Configuration
-*/
-server.listen(process.env.PORT || 5000, () => {
-    console.log("server is running...");
+// -----------------------------------------------------------------------------
+// パラメータ設定
+const line_config = {
+    channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセットしています
+    channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
+};
+
+// -----------------------------------------------------------------------------
+// Webサーバー設定
+server.listen(process.env.PORT || 3000);
+
+// APIコールのためのクライアントインスタンスを作成
+const bot = new line.Client(line_config);
+
+// Dialogflowのクライアントインスタンスを作成
+const session_client = new dialogflow.SessionsClient({
+    project_id: process.env.GOOGLE_PROJECT_ID,
+    credentials: {
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n")
+    }
 });
 
-/*
-** Mount bot-express
-*/
-server.use("/webhook", bot_express({
-    nlu: {
-        type: "dialogflow",
-        language: "ja",
-        options: {
-            project_id: process.env.GOOGLE_PROJECT_ID,
-            client_email: process.env.GOOGLE_CLIENT_EMAIL,
-            private_key: process.env.GOOGLE_PRIVATE_KEY
-        }
-    },
-    memory: {
-        type: "memory-cache",
-        retention: 600
-    },
-    line_channel_secret: process.env.LINE_CHANNEL_SECRET,
-    line_access_token: process.env.LINE_ACCESS_TOKEN,
-    default_skill: process.env.DEFAULT_SKILL,
-    google_project_id: process.env.GOOGLE_PROJECT_ID,
-    auto_translation: process.env.AUTO_TRANSLATION
-}));
+// -----------------------------------------------------------------------------
+// ルーター設定
+server.post('/webhook', line.middleware(line_config), (req, res, next) => {
+    // 先行してLINE側にステータスコード200でレスポンスする。
+    res.sendStatus(200);
 
-module.exports = server;
+    // すべてのイベント処理のプロミスを格納する配列。
+    let events_processed = [];
+
+    // イベントオブジェクトを順次処理。
+    req.body.events.forEach((event) => {
+        // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
+        if (event.type == "message" && event.message.type == "text"){
+            events_processed.push(
+                session_client.detectIntent({
+                    session: session_client.sessionPath(process.env.GOOGLE_PROJECT_ID, event.source.userId),
+                    queryInput: {
+                        text: {
+                            text: event.message.text,
+                            languageCode: "ja",
+                        }
+                    }
+                }).then((responses) => {
+                    if (responses[0].queryResult && responses[0].queryResult.action == "handle-delivery-order"){
+                        let message_text
+                        if (responses[0].queryResult.parameters.fields.menu.stringValue){
+                            message_text = `毎度！${responses[0].queryResult.parameters.fields.menu.stringValue}ね。どちらにお届けしましょ？`;
+                        } else {
+                            message_text = `毎度！ご注文は？`;
+                        }
+                        return bot.replyMessage(event.replyToken, {
+                            type: "text",
+                            text: message_text
+                        });
+                    }
+                })
+            );
+        }
+    });
+
+    // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
+    Promise.all(events_processed).then(
+        (response) => {
+            console.log(`${response.length} event(s) processed.`);
+        }
+    );
+});
